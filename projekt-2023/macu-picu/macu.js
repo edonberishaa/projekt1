cb922e2923ed0bcf3d3eab06aed969fc819bd33b
edonberishaa <150199937+edonberishaa@users.noreply.github.com>
2023-12-26 16:18:54 +0100

    Update macu.js

--- projekt-2023/macu-picu/macu.js
+++ projekt-2023/macu-picu/macu.js
@@ -3,6 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
   const navbarList = document.querySelector('.navbar ul');
 
   hamburgerBtn.addEventListener('click', function () {
-      navbarList.classList.toggle('show');
+    navbarList.classList.toggle('show');
   });
 });
+
+function margjina() {
+  let div = document.getElementById("text-area");
+  if (div.style.margin === "0px 0px 0px 0px") {
+    div.style.margin = "-20% 0px 0px 0px";
+  } else {
+    div.style.margin = "8% 0px 0px 0";
+  }
+}
+
+let div = document.getElementById("hamburger-btn");
+div.addEventListener('click', margjina);

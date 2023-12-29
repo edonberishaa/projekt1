fcee5b611bb34d48a5139cb5df244d32c9ddd1e6
edonberishaa <150199937+edonberishaa@users.noreply.github.com>
2023-12-29 10:34:56 +0100

    Update ica.js

--- projekt-2023/cicen-ica/ica.js
+++ projekt-2023/cicen-ica/ica.js
@@ -1,16 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerBtn = document.getElementById('hamburger-btn');
-    const navbarList = document.querySelector('.navbar ul');
-  
-    hamburgerBtn.addEventListener('click', function () {
-        navbarList.classList.toggle('show');
-    });
-    hamburgerBtn.removeEventListener();
-
-  });
-  
   const hamburgerBtn = document.getElementById('hamburger-btn');
+  const navbarList = document.querySelector('.navbar ul');
   const margina = document.getElementById('sektion');
-  hamburgerBtn.addEventListener('click',function () {
-    margina.style.margin = '150px 0px 0px 0px';
+
+  let isMenuOpen = false;
+
+  hamburgerBtn.addEventListener('click', function () {
+      navbarList.classList.toggle('show');
+      
+      // Toggle margin based on the menu state
+      if (isMenuOpen) {
+          margina.style.margin = '0';
+      } else {
+          margina.style.margin = '150px 0 0 0';
+      }
+
+      // Update the menu state
+      isMenuOpen = !isMenuOpen;
   });
+});
